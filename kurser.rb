45ba3b026d8443dfcb8477b184c60df3836b6746
Patrik Ragnarsson <patrik@starkast.net>
2009-08-19 00:52:06 +0200

    Fetches area, level, name and points

--- kurser.rb
+++ kurser.rb
@@ -5,62 +5,48 @@ require 'open-uri'
 #$url = "http://kdb-5.liu.se/liu/lith/studiehandboken/svkursplan.lasso?&k_budget_year=2007&k_kurskod="
 $url = "http://kdb-5.liu.se/liu/lith/studiehandboken/svkursplan.lasso?&k_budget_year=2009&k_kurskod="
 
-$kurser = %w(TGTU55 TAMS27 TDDC93 TSEA29 TDDB68 TSDT13)
+$kurser = %w(TGTU55 TAMS27 TDDC93 TSEA29 TDDB68)
+
+def mymerge(one, two)
+	hash = {}
+	one.each_with_index do |item,index| 
+		hash[item] = two[index]
+	end
+	return hash
+end
 
 class Course
-	attr_reader :code, :name
+	attr_reader :code, :name, :level, :area, :points
 	def initialize(code)
 		@code = code
 		@name = Course.fetch_name(code)
-		@txtlista = Course.txtlista(code)
-	end
 
-	def self.merge(one, two)
-		hash = {}
-		one.each_with_index do |item,index| 
-			hash[item] = two[index]
-		end
-		return hash
-	end
-	
-	def self.bamse_funktion(code)
+    # fetch stuff
 		doc = Hpricot(open($url + code))
+		elements = doc.search("span.txtbold")
+		@points = (elements/"b")[2].inner_text.strip[0..-4]
 		titles = (doc./"span.txtkursivlista").map { |x| x = x.inner_text }
-		#p titles
 		content = (doc./"span.txtlista").map { |x| x = x.inner_text }
-		#p content
-		$everything = self.merge(titles, content)
-		#p $everything
-		puts $everything["Kurslitteratur: "]
-		puts $everything["Huvudområde: "]
-		
-	end
-	
-	def self.fetch_info(course)
-		doc = Hpricot(open($url + course))
-		(doc/"span.txtbold").each do |element|
-			puts element.inner_html
-		end
-	end
-	
-	def self.txtlista(course)
-		doc = Hpricot(open($url + course))
-		(doc/"span.txtlista").each do |element|
-			p element.inner_html
-			puts ""
-		end
+		$everything = mymerge(titles, content)
+
+    @level = $everything["Huvudområde: "]
+    @area = $everything["Utbildningsområde: "]
 	end
 	
 	def self.fetch_name(course)
 		doc = Hpricot(open($url + course))
 		return doc.search("span.txtbold").at("b").inner_text.split(",")[0]
 	end
+	
+	def pretty_print
+	  puts "#{@code}: #{@name}"
+	  puts "\t Level: #{@level}"
+	  puts "\t Area: #{@area}"
+	  puts "\t Points: #{@points} hp"
+	  puts "---"
+  end
 end
 
-#$kurser.each {|kurs| get_course_name(kurs)}
-
-#bla = Course.new("TAMS27")
-#p bla.name
-
-#Course.bamse_funktion("TAMS27")
-Course.bamse_funktion("TDDC90")
+$kurser.each do |kurs|
+  Course.new(kurs).pretty_print
+end
