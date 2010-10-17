616fcd62ee6d1350877acc8d96bbcefa74d8d896
Patrik Ragnarsson <patrik@starkast.net>
2010-10-17 15:50:05 +0200

    Use RiCal for timezone support in upplys.rb

--- upplys.rb
+++ upplys.rb
@@ -4,10 +4,9 @@ require 'rss/1.0'
 require 'rss/2.0'
 require 'open-uri'
 require 'rubygems'
-require 'icalendar'
-require 'date'
+require 'tzinfo'
+require 'ri_cal'
 
-include Icalendar
 
 #icsfile = "/home/dentarg/www/vhosts/blog.dentarg.net/upplysningar.ics"
 icsfile = "upplysningar.ics"
@@ -16,29 +15,32 @@ content = "" # raw content of rss feed will be loaded here
 open(source) {|s| content = s.read }
 
 rss = RSS::Parser.parse(content, false)
-cal = Calendar.new
 
-cal.custom_property("X-WR-CALNAME;VALUE=TEXT", "UppLYSning")
-cal.custom_property("X-WR-CALDESC;VALUE=TEXT", "Lysators föredragsverksamhet")
+cal = RiCal.Calendar do |cal|
 
-rss.items.each do |item|
-	if item.title.match(/(\d+)\/(\d+): (.+)/)
-		day = $1.to_i
-		month = $2.to_i
-		title = $3
-	end
-		
-	event_start = DateTime.civil(Time.now.year, month, day, 18)
-	event_end = DateTime.civil(Time.now.year, month, day, 20)
-
-	cal.event do
-		dtstart(event_start)
-		dtend(event_end)
-		summary(title)
-		description(item.description)
-		klass("PUBLIC")
-		url(item.link)
-	end
+  #cal.custom_property("X-WR-CALNAME;VALUE=TEXT", "UppLYSning")
+  #cal.custom_property("X-WR-CALDESC;VALUE=TEXT", "Lysators fï¿½redragsverksamhet")
+  cal.default_tzid="Europe/Stockholm"
+  
+  rss.items.each do |item|
+  	if item.title.match(/(\d+)\/(\d+): (.+)/)
+  		day = $1.to_i
+  		month = $2.to_i
+  		title = $3
+  	end
+  		
+  	event_start = Time.utc(Time.now.year, month, day, 18)
+  	event_end = Time.utc(Time.now.year, month, day, 20)
+    
+  	cal.event do |event|
+  		event.description = item.description
+  		event.dtstart = event_start
+  		event.dtend = event_end
+  		event.summary = title		
+  		event.location = "Visionen, B-huset, Campus Valla, LinkÃ¶ping"
+  		event.url = item.link
+  	end
+  end
 end
 
-File.open(icsfile, "w").write(cal.to_ical)
+File.open(icsfile, "w").write(cal.export)
